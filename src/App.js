b9a43a8150e1cf889d7c36088c85defd0daf4ac6
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2020-04-20 16:38:05 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className='max-w-sm rounded-md owerflow-hidden shadow-lg'>
+      
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-red-500 text-xl">Jidlo</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
